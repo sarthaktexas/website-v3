42f6d6d3dc5651cd5f78ec51824e2d47a61cfc7b
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-02-14 01:27:32 -0600

    🚀 add annual review link

--- components/Nav.js
+++ components/Nav.js
@@ -6,13 +6,14 @@ export default function Nav() {
         { href: '/', label: 'home' },
         { href: '/about', label: 'about' },
         { href: '/manifesto', label: 'manifesto' },
+        { href: 'https://www.notion.so/sarthakmohanty/Annual-Review-2020-d8e25a934aa2441fae8cc79a9a1bdbc8', label: 'annual review' },
         //{ href: 'https://medium.com/@sarthaktexas', label: 'writing' },
         //{ href: 'https://twitter.com/sarthaktexas', label: 'thoughts'}
     ]
 
     return (
         <nav>
-            <ul className="flex flex-wrap space-x-5 mt-6 sm:mt-0">
+            <ul className="flex flex-col sm:flex-row sm:space-x-5 mt-6 sm:mt-0">
                 {links.map(({ href, label }) => (
                     <li key={`${href}${label}`} className="list-none pointer-events-auto p-0 m-0">
                         <Link href={href}>
