ebfd82655e6ea05daaf2b1a7c1d188d3d9bf2ec4
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-10-11 14:00:33 -0500

    make input required

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -17,8 +17,8 @@ export default function Newsletter() {
             <form className="rounded p-4 bg-black bg-opacity-20 pointer-events-auto flex flex-col space-y-2 md:w-1/2" onSubmit={handleSubmit(onSubmit)}>
                 <h1 className="font-bold text-lg">Join my newsletter!</h1>
                 <label htmlFor="bd-email" className=" text-white text-opacity-50">Enter your email</label>
-                <input className="text-black rounded p-2 focus:ring-purple-500" ref={register} type="email" name="email" id="bd-email" placeholder="sarthak@sarthakmohanty.me" disabled={formState.isSubmitting} />
-                <input ref={register} type="hidden" defaultValue={1} name="embed" />
+                <input className="text-black rounded p-2 focus:ring-purple-500" type="email" name="email" id="bd-email" placeholder="sarthak@sarthakmohanty.me" disabled={formState.isSubmitting} {...register("email", { required: true })} />
+                <input type="hidden" defaultValue={1} name="embed" {...register("embed", { required: true })} />
                 <button className="a" type="submit" disabled={formState.isSubmitting}>Subscribe</button>
                 {formState.isSubmitted && (
                     <div className="text-green-500">Signed up successfully!</div>
