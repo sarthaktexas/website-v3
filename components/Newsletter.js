9a1185d1e473261bcd0b239fa82be458b2198b16
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-10-11 14:04:30 -0500

    change button into input

--- components/Newsletter.js
+++ components/Newsletter.js
@@ -19,7 +19,7 @@ export default function Newsletter() {
                 <label htmlFor="bd-email" className=" text-white text-opacity-50">Enter your email</label>
                 <input className="text-black rounded p-2 focus:ring-purple-500" type="email" name="email" id="bd-email" placeholder="sarthak@sarthakmohanty.me" disabled={formState.isSubmitting} {...register("email", { required: true })} />
                 <input type="hidden" defaultValue={1} name="embed" {...register("embed", { required: true })} />
-                <button className="a" type="submit" disabled={formState.isSubmitting}>Subscribe</button>
+                <input className="a" type="submit" disabled={formState.isSubmitting} defaultValue="Subscribe" />
                 {formState.isSubmitted && (
                     <div className="text-green-500">Signed up successfully!</div>
                 )}
